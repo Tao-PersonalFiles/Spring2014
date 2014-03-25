39df90d25dbce41d5a8dd4c918e498491e6836f4
ztgentle0910 <beard_china69@hotmail.com>
2014-03-24 17:43:44 -0700

    new

--- CS 383/Implement_Spells/src/implement_spells/Spell.java
+++ CS 383/Implement_Spells/src/implement_spells/Spell.java
@@ -7,13 +7,15 @@
 package implement_spells;
 
 import implement_spells.Spell_Details.*;
-
+import java.awt.BorderLayout;
 import java.awt.FlowLayout;
+
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -28,7 +30,7 @@ public class Spell {
     double  ManaCost;
     
     public JButton spellbutton;
-    private JFrame selectF;
+    private JFrame spellFrame;
     private JPanel controlPanel;
     
     Spell(String n, int lv, double mc) {
@@ -40,16 +42,62 @@ public class Spell {
         spellbutton.addActionListener(new ActionListener(){
             @Override
             public void actionPerformed(ActionEvent e){
-                prepareSelectionGUI();
+                showSpellDetails();
                 showButtonDemo();
             }
         });
     }
     
+    public void showSpellDetails(){
+        spellFrame = new JFrame(Name);
+        spellFrame.setSize(300,500);
+        
+        spellFrame.addWindowListener( new WindowAdapter() {
+            @Override
+            public void windowClosing( WindowEvent e )
+            {  //System.exit(0); 
+
+            }
+        });
+        
+        spellFrame.getContentPane().add( new Spell_Details(Name));
+        spellFrame.setVisible( true );
+    }
+    
+    public void showButtonDemo(){
+        JButton cast_button = new JButton("Cast");
+        JButton cancel_button = new JButton("Cancel");
+        
+        cast_button.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e){
+                //JFrame spell_cast_window = new JFrame(Name);
+                
+            }
+        });
+        
+        cancel_button.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e){
+                spellFrame.dispose();
+            }
+        });
+        
+        JPanel selection = new JPanel();// panel for cast and cancel buttons
+        selection.setLayout(new FlowLayout());
+        selection.add(cast_button);
+        selection.add(cancel_button);
+        
+        spellFrame.setLayout(new BorderLayout());
+        spellFrame.add(selection, BorderLayout.SOUTH);
+        spellFrame.setVisible(true);
+    }
+    
+    /*
     public void prepareSelectionGUI(){
         selectF = new JFrame("Selection");
-        selectF.setSize(350,80);
-        selectF.setLayout(new GridLayout(1,3));
+        selectF.setSize(100,150);
+        //selectF.setLayout(new GridLayout(3,1));
         selectF.addWindowListener(new WindowAdapter() {
            @Override
            public void windowClosing(WindowEvent windowEvent){
@@ -58,7 +106,7 @@ public class Spell {
         }); 
         
         controlPanel = new JPanel();
-        controlPanel.setLayout(new FlowLayout());
+        controlPanel.setLayout(new GridLayout(3,1));
         
         selectF.add(controlPanel);
         selectF.setVisible(true);
@@ -73,21 +121,30 @@ public class Spell {
             @Override
             public void actionPerformed(ActionEvent e){
                 //=========================
-                JFrame spell_window = new JFrame("Spell Details");
+                JFrame spell_detail_window = new JFrame("Spell Details");
+                spell_detail_window.setLayout(new BorderLayout());
                 int sframe_width  = 300;
                 int sframe_height = 500;
         
-                spell_window.addWindowListener( new WindowAdapter() {
+                spell_detail_window.addWindowListener( new WindowAdapter() {
                     @Override
                     public void windowClosing( WindowEvent e )
                     {  //System.exit(0); 
                         
                     }
                 });
-        
-                spell_window.setSize(sframe_width, sframe_height);
-                spell_window.getContentPane().add( new Spell_Details(Name) );
-                spell_window.setVisible( true );
+                
+                spell_detail_window.setSize(sframe_width, sframe_height);
+                spell_detail_window.getContentPane().add( new Spell_Details(Name) );
+                
+                JPanel selection = new JPanel();
+                selection.setLayout(new FlowLayout());
+                JButton cast_button = new JButton("Cast");
+                selection.add(cast_button);
+                selection.add(new JButton("Cancel"));
+                spell_detail_window.add(selection, BorderLayout.SOUTH);
+                
+                spell_detail_window.setVisible( true );
                 
             }
         });
@@ -103,7 +160,7 @@ public class Spell {
         cancelButton.addActionListener(new ActionListener(){
             @Override
             public void actionPerformed(ActionEvent e){
-                
+                selectF.dispose();
             }
         });
         
@@ -113,4 +170,5 @@ public class Spell {
         
         selectF.setVisible(true);
     }
+    */
 }
